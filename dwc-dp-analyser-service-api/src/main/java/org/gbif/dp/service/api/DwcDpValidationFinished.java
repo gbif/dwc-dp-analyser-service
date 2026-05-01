c19ecf9e46df6cf1c585b1a0fffa2bff5d6870f9
Nikolaj Schaldemose Reibke <nreibke@gbif.org>
2026-05-01 10:23:36 +0200

    converted message class to from record->class to allow inheritance

--- dwc-dp-analyser-service-api/src/main/java/org/gbif/dp/service/api/DwcDpValidationFinished.java
+++ dwc-dp-analyser-service-api/src/main/java/org/gbif/dp/service/api/DwcDpValidationFinished.java
@@ -2,11 +2,66 @@ package org.gbif.dp.service.api;
 
 import org.gbif.dp.analysis.api.DatapackageAnalysisResult;
 
-public record DwcDpValidationFinished(
-  String datasetUuid,
-  int attempt,
-  Boolean valid,
-  DatapackageAnalysisResult validationReport
-) {
+import java.util.Objects;
+
+public class DwcDpValidationFinished {
+  private final String datasetUuid;
+  private final int attempt;
+  private final Boolean valid;
+  private final DatapackageAnalysisResult validationReport;
+
+  public DwcDpValidationFinished(
+    String datasetUuid,
+    int attempt,
+    Boolean valid,
+    DatapackageAnalysisResult validationReport
+  ) {
+    this.datasetUuid = datasetUuid;
+    this.attempt = attempt;
+    this.valid = valid;
+    this.validationReport = validationReport;
+  }
+
+  public String datasetUuid() {
+    return datasetUuid;
+  }
+
+  public int attempt() {
+    return attempt;
+  }
+
+  public Boolean valid() {
+    return valid;
+  }
+
+  public DatapackageAnalysisResult validationReport() {
+    return validationReport;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (DwcDpValidationFinished) obj;
+    return Objects.equals(this.datasetUuid, that.datasetUuid) &&
+      this.attempt == that.attempt &&
+      Objects.equals(this.valid, that.valid) &&
+      Objects.equals(this.validationReport, that.validationReport);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(datasetUuid, attempt, valid, validationReport);
+  }
+
+  @Override
+  public String toString() {
+    return "DwcDpValidationFinished[" +
+      "datasetUuid=" + datasetUuid + ", " +
+      "attempt=" + attempt + ", " +
+      "valid=" + valid + ", " +
+      "validationReport=" + validationReport + ']';
+  }
+
 
 }
