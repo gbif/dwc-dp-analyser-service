669fc315e8fe66b83e574af796dc62d6ccad1ac8
Nikolaj Schaldemose Reibke <nreibke@gbif.org>
2026-06-15 12:55:56 +0200

    chore: mvn spotless:apply

--- dwc-dp-analyser-service-app/src/main/java/org/gbif/dp/service/Main.java
+++ dwc-dp-analyser-service-app/src/main/java/org/gbif/dp/service/Main.java
@@ -1,8 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gbif.dp.service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.rabbitmq.client.Channel;
 
 import org.gbif.dp.analysis.DefaultDataPackageAnalysisOrchestrator;
 import org.gbif.dp.analysis.api.DataAnalyser;
@@ -16,11 +25,13 @@ import org.gbif.dp.duckdb.DuckDbConfig;
 import org.gbif.dp.duckdb.DuckDbConfigBuilder;
 import org.gbif.dp.service.messaging.rabbitmq.RabbitMessageConsumer;
 import org.gbif.dp.service.messaging.rabbitmq.RabbitMessagePublisher;
-
 import org.gbif.dp.service.messaging.rabbitmq.RabbitMqConnection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 
 import picocli.CommandLine;
 
--- dwc-dp-analyser-service-app/src/test/java/org/gbif/dp/service/DwcValidatorTest.java
+++ dwc-dp-analyser-service-app/src/test/java/org/gbif/dp/service/DwcValidatorTest.java
@@ -1,17 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gbif.dp.service;
 
-import org.gbif.dp.analysis.api.ValidationOptions;
 import org.gbif.dp.analysis.api.DatapackageAnalysisResult;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
+import org.gbif.dp.analysis.api.ValidationOptions;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.*;
 
